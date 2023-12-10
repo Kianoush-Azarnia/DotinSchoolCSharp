a39d4eaf14ed3d52f4af6a56e5f9a97ed90bdef3
Kianoush Azarnia <71564764+Kianoush-Azarnia@users.noreply.github.com>
2023-12-10 12:20:47 +0330

    Add project files.

---
+++ MyFirstConsoleApp/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using dataHelper;
+
+namespace MyFirstConsoleApp
+{
+    class Program
+    {
+        static void Main(String[] args)
+        {
+            var csv = "John,Doe,M,25\nJane,Doe,F,30\nBob,Smith,M,40\nAlice,Smith,F,35";
+            var (maleAvgAge, femaleAvgAge) = dataHelper.DataHelper.GetAverageAgeBySex(csv);
+
+            Console.WriteLine($"Male average age: {maleAvgAge}");
+            Console.WriteLine($"Female average age: {femaleAvgAge}");
+
+        }
+
+}
+
+}
+
---
+++ Useful_Tools/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace dataHelper
+{
+    public class DataHelper
+    {
+        public static (double maleAvgAge, double femaleAvgAge) GetAverageAgeBySex(string csv)
+        {
+            var rows = csv.Split('\n');
+            var maleAges = new List<int>();
+            var femaleAges = new List<int>();
+
+            foreach (var row in rows)
+            {
+                var cols = row.Split(',');
+                if (cols.Length != 4) continue;
+
+                var firstName = cols[0];
+                var lastName = cols[1];
+                var sex = cols[2];
+                var age = int.Parse(cols[3]);
+
+                if (sex == "M")
+                {
+                    maleAges.Add(age);
+                }
+                else if (sex == "F")
+                {
+                    femaleAges.Add(age);
+                }
+            }
+
+            var maleAvgAge = maleAges.Count > 0 ? maleAges.Average() : 0;
+            var femaleAvgAge = femaleAges.Count > 0 ? femaleAges.Average() : 0;
+
+            return (maleAvgAge, femaleAvgAge);
+        }
+    }
+}
+
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        
+    }
+}
