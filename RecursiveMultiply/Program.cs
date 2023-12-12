d4472a036b0285f269ae62df44a579f5c0a679d0
Kianoush Azarnia <71564764+Kianoush-Azarnia@users.noreply.github.com>
2023-12-12 23:22:49 +0330

    organizing / new excercises

---
+++ RecursiveMultiply/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace RecMultiply
+{
+    class Program
+    {
+        // A function that takes two integers and returns their product using addition and recursion
+        static int Multiply(int a, int b)
+        {
+            // Base case: if either a or b is zero, the product is zero
+            if (a == 0 || b == 0)
+            {
+                return 0;
+            }
+
+            // Recursive case: if b is positive, add a to the product of a and b-1
+            if (b > 0)
+            {
+                return a + Multiply(a, b - 1);
+            }
+
+            // Recursive case: if b is negative, subtract a from the product of a and b+1
+            if (b < 0)
+            {
+                return -a + Multiply(a, b + 1);
+            }
+
+            // This line should never be reached
+            return 0;
+        }
+
+        static void Main(string[] args)
+        {
+            // Ask the user for a and b
+            Console.WriteLine("Please enter the first number:");
+            int a = int.Parse(Console.ReadLine());
+            Console.WriteLine("Please enter the second number:");
+            int b = int.Parse(Console.ReadLine());
+
+            // Call the multiply function with a and b
+            int result = Multiply(a, b);
+
+            // Print the result
+            Console.WriteLine($"The product of {a} and {b} is {result}.");
+        }
+    }
+}
---
+++ RecursiveVerticalNumPrinter/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic; // To use the Stack class
+
+namespace PrintVertical
+{
+    class Program
+    {
+        // A stack to store the digits of the number
+        static Stack<int> digits = new Stack<int>();
+
+        // A recursive function that takes a number and pushes its digits to the stack
+        static void PushDigits(int num)
+        {
+            // Base case: if the number is zero, do nothing
+            if (num == 0)
+            {
+                return;
+            }
+
+            // Recursive case: push the last digit of the number to the stack and then call the function with the number divided by 10
+            digits.Push(num % 10);
+            PushDigits(num / 10);
+        }
+
+        // A function that pops and prints the digits from the stack
+        static void PrintDigits()
+        {
+            // Loop until the stack is empty
+            while (digits.Count > 0)
+            {
+                // Pop and print the top digit from the stack
+                Console.WriteLine(digits.Pop());
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            // Ask the user for a number
+            Console.WriteLine("Please enter a number:");
+            int num = int.Parse(Console.ReadLine());
+
+            // Call the push digits function with the number
+            PushDigits(num);
+
+            // Call the print digits function
+            PrintDigits();
+        }
+    }
+}
